f9f11306952c512bb8032cd222a6f4fb142477b7
liangkj <liangkaijun52013@gmail.com>
2017-01-19 18:57:21 +0800

    2017.11.19

--- public/javascripts/fetch_unsplash_image.js
+++ public/javascripts/fetch_unsplash_image.js
@@ -1,7 +1,14 @@
-/**
- * Created by liangkj on 2017/1/18.
- */
+const flowAction = require('./flow_action.js');
+console.log(flowAction);
+var af = flowAction.ActionFactory;
 
+var requestUrl = 'https://source.unsplash.com/random/50/50';
+var action = af.getAction('rf', requestUrl, '', (allData) => {
+    console.log(allData);
+});
+if (action){
+    action.selfDo();
+}
 
 
 
--- public/javascripts/flow_action.js
+++ public/javascripts/flow_action.js
@@ -7,46 +7,37 @@ var http = require('http');
 var https = require('https');
 
 function getHttpModule (protocol) {
-    if (protocol == 'https')
+    if (protocol.startsWith('https'))
         return https;
     return http;
 }
 
-function doAction(url, options) {
-    var realAction = ActionFactory.getAction(ActionFactory.parse(url, options));
-    realAction.selfDo();
-}
-
-
 var ActionFactory = {
-    reg : /([a-zA-Z]+):\\\\\\(.*)/,
-    parse: function (url) {
-        return url.match(this.reg);
-    },
-
-    getAction:function (array, options) {
-        var actionName = array && array.length >= 1 ? array[0] : '';
-        var realUrl = array && array.length >= 2 ? array[1] : '';
-        var orignParams = array && array.length >= 3 ? array[2] : '';
+    //reg : /([a-zA-Z]+):\\\\\\(.*)/,
+    // parse: function (url) {
+    //     return url.match(this.reg);
+    // },
+    getAction:function (actionName, url, options, call) {
         if (actionName == 'rf'){
-            return new RequestFetchAction(realUrl, orignParams, options);
+            return new RequestFetchAction(url, options, call);
         }else if (actionName == 'fs'){
-            return new FileSaveAction(realUrl, orignParams, options);
+            return new FileSaveAction(url, options, call);
         }
 
-        return new BaseAction(realUrl, orignParams, options);
+        return new BaseAction(url, options, call);
     }
 };
 
-function BaseAction(url, params, options) {
-    function selfDo() {
-        UTIL.log(url, params, options);
+function BaseAction(url, options, call) {
+    BaseAction.prototype.selfDo = function () {
+        UTIL.log(url, options, call);
         return null;
     }
 };
 
-function RequestFetchAction(url, params, options) {
-    function selfDo() {
+function RequestFetchAction(url, options, call) {
+    BaseAction.call(this);
+    this.selfDo = function () {
         if (!checkParams()){
             return null;
         }
@@ -64,45 +55,49 @@ function RequestFetchAction(url, params, options) {
         var req = protocol.request(requestOptions, function (res) {
             res.on('data', function (chunk) {
                 allData += chunk;
-            })
+                UTIL.log(`chunk size...${chunk.length}`);
+            });
+
+            res.on('end', call);
         });
 
         req.on('error', function (error) {
-            UTIL.error(error);
+            console.error(error);
         });
-
         req.end();
-        return allData;
     }
 
     function checkParams() {
         if (! url){
-            UTIL.log(`url wrong...origions: ${params}, options: ${options}`);
+            UTIL.log(`url wrong...url: ${url}, options: ${options}`);
             return false;
         }
 
         var parse = URL.parse(url, true);
         if (! parse){
-            UTIL.log(`url parse wrong...origions: ${params}, options: ${options}`);
+            UTIL.log(`url parse wrong...url: ${url}, options: ${options}`);
             return false;
         }
 
         if (! parse.protocol){
-            UTIL.log(`protocol wrong...origions: ${params}, options: ${options}`);
+            UTIL.log(`protocol wrong...url: ${url}, options: ${options}`);
             return false;
         }
         return true;
     }
 }
 
-function FileSaveAction(url, params, options) {
+function FileSaveAction(url, options, call) {
+    BaseAction.call(this);
     var fs = require('fs');
-    function selfDo() {
+    this.selfDo = function () {
         if (! checkParams(url, options)){
             return false;
         }
 
-        fs.writeFile();
+        var data = options.data;
+        options.delete(options.data);
+        fs.writeFile(url, data, options, call);
     }
 
     function checkParams() {
@@ -110,7 +105,12 @@ function FileSaveAction(url, params, options) {
             UTIL.log(`url wrong...${url}, ${options}`);
             return false;
         }
+
+        if (!options.data){
+            UTIL.log(`options data wrong...${url}, ${options}`);
+            return false;
+        }
         return true;
     }
 }
-
+exports.ActionFactory = ActionFactory;
