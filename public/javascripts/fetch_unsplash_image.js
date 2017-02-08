3ab08fd547e9c896db7fd398b9fd1d28284a239d
liangkj <liangkaijun52013@gmail.com>
2017-02-08 13:52:57 +0800

    2017.1.23

--- public/javascripts/fetch_unsplash_image.js
+++ public/javascripts/fetch_unsplash_image.js
@@ -9,21 +9,6 @@ var firstFetch = af.getAction('rf', args);
 var startNode = Object.create(WorkNode);
 startNode.action = firstFetch;
 
-
-// var secondFetch = af.getAction('rf', {encoding: 'binary'});
-// var secondNode = Object.create(WorkNode);
-// secondNode.action = secondFetch;
-// secondNode.action.argsDeal = function (args) {
-//     var reg = /href\s*=\s*"(.*)"/;
-//     var match = args.data.match(reg);
-//     if (match){
-//         secondNode.action.args.url = match[1];
-//     }
-// };
-// startNode.next = secondNode;
-//
-//
-console.log(`Current directory: ${process.cwd()}`);
 var thirdNode = Object.create(WorkNode);
 var timestamp = new Date().valueOf();
 var args = { url: 'public/images/'+timestamp+'.jpg', encoding: 'binary'};
